426d4f2bbdb04f3103dc38262456b0cf7e5ed52e
Pavel <92167532+P4wtet@users.noreply.github.com>
2023-07-12 23:35:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("burger").addEventListener("click",function(){document.querySelector("header").classList.toggle("open")})});
